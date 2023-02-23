a3a48c2c35df3990a29c419b044c89b470245be7
starmickey <micaelaestrellamorales@gmail.com>
2023-02-23 11:47:50 -0300

    Create Item component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Item from "./Item";
 
 function List(props) {
     return (<ul>
-        {props.items.map((item, index) => <li key={index}>{item}</li>)}
+        {props.items.map((item, index) => <Item key={index} value={item} />)}
      </ul>)
 }
 
