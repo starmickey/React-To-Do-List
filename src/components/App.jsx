730e5c96d4f46e7a134a42ce63a52b5293c7e8e9
starmickey <micaelaestrellamorales@gmail.com>
2023-02-23 11:30:58 -0300

    Create functional list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import List from "./List";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
+   const [items, setItems] = useState([]);
+
+   function handleSubmit(event) {
+      const newItem = event.target[0].value;
+      
+      setItems(prevItems => [...prevItems, newItem]);
+      event.preventDefault();
+   }
+
+   return (
+      <div className="container">
+         <div className="heading">
+            <h1>To-Do List</h1>
+         </div>
+         <form onSubmit={handleSubmit}>
+            <input type="text" />
+            <button>
+               <span>Add</span>
+            </button>
+         </form>
+         <div>
+            <List items={items} />
+         </div>
       </div>
-      <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          <li>A Item</li>
-        </ul>
-      </div>
-    </div>
-  );
+   );
 }
 
 export default App;
